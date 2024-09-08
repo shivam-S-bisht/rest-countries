a068e95e5752b08633477a98358e41129cd15cb1
Shivam <bishtshivam096@gmail.com>
2024-09-08 21:04:29 +0530

    using height, previous logic seems more comples

--- src/lib/components/SearchBox/index.tsx
+++ src/lib/components/SearchBox/index.tsx
@@ -1,11 +1,9 @@
-import { ChangeEvent, Fragment, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, Fragment, useState } from 'react';
 import styles from './styles.module.scss';
 import { getCountriesByName } from '@/lib/utils/apis';
 
 function SearchBox() {
 	const [data, setData] = useState<ICountriesData[]>();
-	const resultsWrapperRef = useRef<HTMLDivElement>(null);
-	const resultsCardRef = useRef<HTMLDivElement>(null);
 
 	async function handleChange(
 		event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
@@ -14,17 +12,6 @@ function SearchBox() {
 		const countries = await getCountriesByName(input);
 		setData(countries);
 	}
-
-	useEffect(() => {
-		let itemCount = 10; // Initial number of items
-		if (data && data?.length < 10) itemCount = data.length;
-		if (!resultsCardRef.current || !resultsWrapperRef.current) return;
-
-		const resultCardOffsetHeight = resultsCardRef.current.offsetHeight;
-		resultsWrapperRef.current.style.height = `${
-			resultCardOffsetHeight * itemCount
-		}px`; // Set height based on 10 items
-	}, [data, resultsCardRef.current, resultsWrapperRef.current]);
 
 	return (
 		<div
@@ -35,19 +22,18 @@ function SearchBox() {
 				<input
 					onChange={handleChange}
 					type="text"
-					placeholder="Search"
+					placeholder="Search Countries"
 					className="form-control-plaintext"
 				/>
 			</div>
 			<div
-				ref={resultsWrapperRef}
 				className={`${styles.searchResultsWrapper} d-flex flex-column`}
 			>
 				{data &&
 					data.length > 0 &&
 					data.map((item, index) => {
 						return (
-							<div ref={resultsCardRef} key={index}>
+							<Fragment key={index}>
 								<div className={styles.hr} />
 								<div
 									className={`${styles.resultCard} d-flex align-items-center gap-2 p-2`}
@@ -60,7 +46,7 @@ function SearchBox() {
 									></div>
 									<p className="m-0">{item.name.official}</p>
 								</div>
-							</div>
+							</Fragment>
 						);
 					})}
 			</div>
