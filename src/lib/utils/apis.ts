bbda6276ea9eaf89dab1528e9180ffbd19a61baf
Shivam <bishtshivam096@gmail.com>
2024-09-10 00:02:03 +0530

    added abort controller in the apis

--- src/lib/utils/apis.ts
+++ src/lib/utils/apis.ts
@@ -1,11 +1,16 @@
+let abortController: AbortController;
+
 export async function getCountriesByName(
 	term?: string,
 ): Promise<ICountriesData[]> {
+	abortController?.abort();
 	if (!term) return [];
 
 	try {
+		abortController = new AbortController();
 		const resp = await fetch(
 			`${process.env.NEXT_PUBLIC_API_URL}/v3.1/name/${term}`,
+			{ signal: abortController.signal },
 		);
 		const countries = await resp.json();
 		if (resp.status < 300) {
